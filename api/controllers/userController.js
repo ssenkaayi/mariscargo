33df23a41fd0d92308af3e0e14a9405bbb2581c7
ssenkaayi <ssenkaaayi@gmail.com>
2024-06-19 11:50:58 +0300

    increasing token expration period

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -40,7 +40,7 @@ export const registerEmploye =(async(req,res,next)=>{
 const generateToken = (id)=>{
 
     // it will be expired after 120ms
-    return Jwt.sign({_id:id},process.env.JWT_SECRET,{expiresIn:60})
+    return Jwt.sign({_id:id},process.env.JWT_SECRET,{expiresIn:"6h"})
 }
 
 export const loginEmploye = async(req,res,next)=>{
