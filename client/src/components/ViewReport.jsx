e7c8d8c19f1c3036e30113c6112e8675a88cd9b5
ssenkaayi <ssenkaaayi@gmail.com>
2024-07-12 14:02:26 +0300

    refactor client and supplier report

--- client/src/components/ViewReport.jsx
+++ client/src/components/ViewReport.jsx
@@ -8,7 +8,7 @@ export default function ViewReport(props) {
     
     <div className='grid grid-rows-11 p-2 gap-3 w-full h-full rounded-lg bg-slate-100'>
 
-        <div className='centered row-span-3 bg-white p-2 rounded-lg '>
+        <div className='centered row-span-10 bg-white p-2 rounded-lg '>
 
             <div className='mb-3 mt-2 text-xl'>
                 
@@ -22,59 +22,62 @@ export default function ViewReport(props) {
                 <p className='flex gap-8'><span className='w-50'>Total No Trips:</span><span></span>{reportDetails.number}</p >
             </div>
 
+            <table className='w-full bordered hover mt-5'>
+
+                <thead className='bg-slate-300'>
+
+                    <tr>
+
+                    {tableHeading.map((item,index)=>{
+
+                        return(                    
+                        <th className='p-2 text-center'key={index}>{item}</th>
+
+                        )
+                    })}
+
+                    </tr>
+
+                </thead >
+
+                <tbody className='p-1'>
+
+                    {tableData.length>0?tableData.map((client,index)=>{
+
+                    return(  
+                        
+                        <tr className='text-center ' key={client._id}>
+
+                        <td className='p-2'>{client.date.length>0?client.date.split("T",1):"no date"}</td>
+                        <td >{client.name}</td>
+                        <td >{client[column1]}</td>
+                        <td >{client[column2]}</td>
+                        <td >{client[column3]}</td>
+
+                        </tr>                  
+
+                    )
+                    }):<tr className='text-center'>
+
+                    <td className='text-center'>{title} not available </td>
+
+                    </tr>}
+
+                </tbody>
+
+            </table>
+
         </div>
 
-        <div  className='centered text-x row-span-8 bg-white p-2 rounded-lg '>
+        <div  className='centered text-x row-span-1 bg-white p-2 rounded-lg '>
         
             <div>
 
-                <table className='w-full bordered hover'>
 
-                    <thead className='bg-slate-300'>
-
-                        <tr>
-
-                        {tableHeading.map((item,index)=>{
-
-                            return(                    
-                            <th className='p-2 text-center'key={index}>{item}</th>
-
-                            )
-                        })}
-
-                        </tr>
-
-                    </thead >
-
-                    <tbody className='p-1'>
-
-                        {tableData.length>0?tableData.map((client,index)=>{
-
-                        return(  
-                            
-                            <tr className='text-center ' key={client._id}>
-
-                            <td className='p-2'>{client.date.length>0?client.date.split("T",1):"no date"}</td>
-                            <td >{client.name}</td>
-                            <td >{client[column1]}</td>
-                            <td >{client[column2]}</td>
-                            <td >{client[column3]}</td>
-
-                            </tr>                  
-
-                        )
-                        }):<tr className='text-center'>
-
-                        <td className='text-center'>{title} not available </td>
-
-                        </tr>}
-
-                    </tbody>
-
-                </table>
                 
             </div>
         </div>
+
     </div>
   )
 }
--- client/src/pages/SupplierReport.jsx
+++ client/src/pages/SupplierReport.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { useState } from 'react';
+import ViewReport from '../components/ViewReport';
+import { tripTable } from '../data/TableHeading';
 
 export default function SupplierReport() {
+
+    // const params = useParams() 
+    const [loading,setLoading] = useState(false);
+    const [error,setError] = useState(null);
+    const [reportData,setReportData] = useState([])
+    const [tripDetails,setTripDetails] = useState({number:0,totalExpense:0,totalTrip_Payment:0,totalWeight:0})
+  
+    // const year = params.year
+    // const month = params.month
+    // const type = params.type
+  
+    const title = `SUPPLIER REPORT FOR  `
+    //   IN ${month} OF ${year}
+
   return (
-    <div>SupplierReport</div>
+
+    <div>
+
+        < ViewReport reportDetails={tripDetails} tableData={reportData} tableHeading={tripTable} column1='weight'
+        column2='expense' column3='trip_payment' title={title}/>
+
+    </div>
   )
 }
