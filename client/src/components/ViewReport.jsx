636d080e2fb6fe0c90a86f71d668f6bba64bf872
ssenkaayi <ssenkaaayi@gmail.com>
2024-08-01 11:08:33 +0300

    adding total to the report summary

--- client/src/components/ViewReport.jsx
+++ client/src/components/ViewReport.jsx
@@ -30,19 +30,13 @@ export default function ViewReport(props) {
             </div>
 
             <div className='m-2 text-x flex gap-4'>
-                {/* <p className='flex gap-8'><span className='w-50'>Weight:</span><span>{reportDetails.totalWeight.toFixed(2)}</span></p >
-                <p className='flex gap-8'><span className='w-50'> {details1}:</span><span>{detailsData1}</span></p >
-                <p className='flex gap-8'><span className='w-50'> {details2}:</span><span>{detailsData2}</span></p >
-                <p className='flex gap-8'><span className='w-50'>No {details3}:</span><span></span>{reportDetails.number}</p > */}
-
 
                 <div className='flex  flex-col'>
 
-                    {/* hello */}
-                    <span className='w-50'>Weight:</span>
-                    <span className='w-50'> {details1}:</span>
-                    <span className='w-50'> {details2}:</span>
-                    <span className='w-50'>No {details3}:</span>
+                    <span className='w-50'>Total Weight:</span>
+                    <span className='w-50'>Total  {details1}:</span>
+                    <span className='w-50'>Total  {details2}:</span>
+                    <span className='w-50'>Total No {details3}:</span>
 
                 </div>
 
@@ -53,9 +47,6 @@ export default function ViewReport(props) {
                     <span>{detailsData2} kgs</span>
                     {reportDetails.number}
                     
-
-                    {/* world */}
-
                 </div>
             </div>
 
